9bacbf4d77ab9ae8a26d45f12e0e700656b2f955
Bert Roos <bert@famroos.nu>
2015-10-29 07:48:13 +0100

    Commented the SpringFox code, as that creates a dependency on Spring HATEOAS 0.18.0, which causes an AbstractMethodError, see issue #407 (https://github.com/spring-projects/spring-hateoas/issues/407). To make reproduction of that issue easy, an explicit dependency on Spring HATEOAS 0.17.0 is added

--- src/main/java/nu/yona/server/YonaServerApplication.java
+++ src/main/java/nu/yona/server/YonaServerApplication.java
@@ -7,50 +7,50 @@
  *******************************************************************************/
 package nu.yona.server;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static springfox.documentation.schema.AlternateTypeRules.newRule;
+//import static com.google.common.collect.Lists.newArrayList;
+//import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
-import java.util.List;
+//import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.RelProvider;
-import org.springframework.hateoas.Resources;
+//import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
-import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fasterxml.classmate.TypeResolver;
+//import com.fasterxml.classmate.TypeResolver;
 
 import nu.yona.server.entities.RepositoryProvider;
-import nu.yona.server.goals.rest.GoalController.GoalResource;
-import nu.yona.server.goals.service.GoalDTO;
+//import nu.yona.server.goals.rest.GoalController.GoalResource;
+//import nu.yona.server.goals.service.GoalDTO;
 import nu.yona.server.rest.JsonRootRelProvider;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.AlternateTypeRule;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.builders.ResponseMessageBuilder;
+//import springfox.documentation.schema.AlternateTypeRule;
+//import springfox.documentation.schema.ModelRef;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 @EnableHypermediaSupport(type = HypermediaType.HAL)
 public class YonaServerApplication {
-
+/*
 	@Autowired
 	private TypeResolver typeResolver;
-
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(YonaServerApplication.class, args);
 	}
-
+/*
 	@Bean
 	public Docket yonaApi() {
 		ApiInfo apiInfo = new ApiInfo("Yona Server", "Server backing the Yona mobile app", "1.0", null, null, "MPL",
@@ -64,7 +64,7 @@ public class YonaServerApplication {
 				.globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
 						.message("500 message").responseModel(new ModelRef("Error")).build()));
 	}
-
+*/
 	@Bean
 	RelProvider relProvider() {
 		return new JsonRootRelProvider();
