79f12a711e46d7fc85a250293760812b1e48b1a5
aristotelos <arisvd@gmail.com>
2015-11-16 16:25:22 +0100

    yd-50 Using sophisticated pluralization
    Fixed test failing after previous commit

--- adminservice/src/intTest/groovy/nu/yona/server/InitialGoalCreationTest.groovy
+++ adminservice/src/intTest/groovy/nu/yona/server/InitialGoalCreationTest.groovy
@@ -57,7 +57,7 @@ class InitialGoalCreationTest extends Specification {
 		then:
 			response.status == 200
 			response.responseData._links.self.href == baseURL + yonaServer.GOALS_PATH
-			response.responseData._embedded.Goals.size() == 2
+			response.responseData._embedded.goals.size() == 2
 			
 	}
 }
