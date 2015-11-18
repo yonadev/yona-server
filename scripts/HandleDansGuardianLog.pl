eaf9dd912bc5ca707fe8ba92dded058a647c4381
Bert Roos <bert@famroos.nu>
2015-11-18 10:10:35 +0100

    YD-99 The script now only uses the analysis engine URL, to fetch the relevant categories and post the potential conflicts

--- scripts/HandleDansGuardianLog.pl
+++ scripts/HandleDansGuardianLog.pl
@@ -38,26 +38,25 @@ sub transform_log_record ($) {
 }
 
 sub fetch_relevant_url_categories {
-	my ($goals_url) = @_;
-	my $get_result = $ua->request(GET $goals_url);
+	my ($analysis_engine_url) = @_;
+	my ($relevant_categories_url) = "${analysis_engine_url}relevantCategories/";
+	my $get_result = $ua->request(GET $relevant_categories_url);
 	my $status_code = $get_result->{'_rc'};
 	if ($status_code != 200) {
-		print STDERR "ERROR: GET on '$goals_url' returned status $status_code\n";
+		print STDERR "ERROR: GET on '$relevant_categories_url' returned status $status_code\n";
 		return;
 	}
-	my $goals_json = $get_result->content;
-	my $goals = decode_json $goals_json;
-	foreach $goal (values $goals->{'_embedded'}->{'Goals'}) {
-		foreach $category (values $goal->{'categories'}) {
-			$relevant_url_categories{$category} .= "";
-		}
+	my $categories_json = $get_result->content;
+	my $categories = decode_json $categories_json;
+	foreach $category (values $categories->{'categories'}) {
+		$relevant_url_categories{$category} .= "";
 	}
 }
 
 sub handle_records_from_stream {
-	my ($fh, $goals_url, $analysis_engine_url) = @_;
+	my ($fh, $analysis_engine_url) = @_;
 
-	fetch_relevant_url_categories $goals_url;
+	fetch_relevant_url_categories $analysis_engine_url;
 	while (<$fh>) {
 		my $analysis_event_json = transform_log_record $_;
 
@@ -72,24 +71,23 @@ sub handle_records_from_stream {
 }
 
 sub handle_records_from_file {
-	my ($file_name, $goals_url, $analysis_engine_url) = @_;
+	my ($file_name, $analysis_engine_url) = @_;
 	open INPUT_FILE, "<$file_name" or die "Cannot open file $INPUT_FILE\n";
-	handle_records_from_stream(*INPUT_FILE, $goals_url, $analysis_engine_url);
+	handle_records_from_stream(*INPUT_FILE, $analysis_engine_url);
 	close INPUT_FILE;
 }
 
-my $goals_url = 'http://localhost:8080/goals/';
-my $analysis_engine_url = 'http://localhost:8080/analysisEngine/';
-GetOptions ('goalsURL=s' => \$goals_url, 'analysisEngineURL=s' => \$analysis_engine_url) or die "Usage: $0 [--goalsURL <URL>] [--analysisEngineURL <URL>] [<input file>]";
+my $analysis_engine_url = 'http://localhost:8081/analysisEngine/';
+GetOptions ('analysisEngineURL=s' => \$analysis_engine_url) or die "Usage: $0 [--analysisEngineURL <URL>] [<input file>]";
 my $input_file = $ARGV[0];
 if ($input_file) {
 	if (! -e $input_file) {
 		die "Input file '$input_file' does not exist\n";
 	}
 	do {
-		handle_records_from_file($input_file, $goals_url, $analysis_engine_url);
+		handle_records_from_file($input_file, $analysis_engine_url);
 	} while (-p $input_file);
 	
 } else {
-	handle_records_from_stream(STDIN, $goals_url, $analysis_engine_url);
+	handle_records_from_stream(STDIN, $analysis_engine_url);
 }
