d7f43cf1385c65f49ecebed0ca02b41ef6baf7b5
aristotelos <arisvd@gmail.com>
2015-11-25 20:37:46 +0100

    YD-105 Removed unneccesary code

--- core/src/main/java/nu/yona/server/subscriptions/entities/Buddy.java
+++ core/src/main/java/nu/yona/server/subscriptions/entities/Buddy.java
@@ -4,24 +4,17 @@
  *******************************************************************************/
 package nu.yona.server.subscriptions.entities;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import nu.yona.server.crypto.StringFieldEncrypter;
 import nu.yona.server.crypto.UUIDFieldEncrypter;
 import nu.yona.server.entities.EntityWithID;
 import nu.yona.server.entities.RepositoryProvider;
-import nu.yona.server.goals.entities.Goal;
 import nu.yona.server.subscriptions.entities.BuddyAnonymized.Status;
 
 @Entity
@@ -44,13 +37,6 @@ public class Buddy extends EntityWithID
 
 	@Convert(converter = StringFieldEncrypter.class)
 	private String nickName;
-
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Convert(converter = UUIDFieldEncrypter.class)
-	private Set<UUID> goalIDs;
-
-	@Transient
-	private Set<Goal> goals = new HashSet<>();
 
 	// Default constructor is required for JPA
 	public Buddy()
@@ -96,16 +82,6 @@ public class Buddy extends EntityWithID
 	public User getUser()
 	{
 		return User.getRepository().findOne(userID);
-	}
-
-	public void setGoalNames(Set<String> goalNames)
-	{
-		goals = goalNames.stream().map(Goal.getRepository()::findByName).collect(Collectors.toSet());
-	}
-
-	public void setGoals(Set<Goal> goals)
-	{
-		goals = new HashSet<>(goals);
 	}
 
 	public UUID getLoginID()
