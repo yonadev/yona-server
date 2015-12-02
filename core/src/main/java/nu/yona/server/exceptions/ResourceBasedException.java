a4098d35ed7c516fbc4d142f3f0d5027555c27eb
pgussow <pgussow@2g.nl>
2015-12-02 12:50:30 +0100

    Optimized imports

--- core/src/main/java/nu/yona/server/exceptions/ResourceBasedException.java
+++ core/src/main/java/nu/yona/server/exceptions/ResourceBasedException.java
@@ -1,9 +1,5 @@
 package nu.yona.server.exceptions;
 
-import java.util.Locale;
-
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 
 /**
