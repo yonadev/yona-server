53ee63b4fe6ba32352b58d989f8456729c1ba7ae
Bert Roos <bert@famroos.nu>
2015-12-05 08:51:02 +0100

    Improved database initialization
    * The servers_start scripts now first do a synchronous build of Yona
    * Next, they synchronously run dbinit
    * Only if that is successful, they start the HSQLDB and the services
    * The services only validate the schema, they don't try to update it anymore
    * dbinit is (both from Gradle and Docker) explicitly requested to update the database schema
    * The GoalFileLoader now also logs successful completion

--- servers_start.cmd
+++ servers_start.cmd
@@ -4,8 +4,10 @@ call servers_stop.cmd
 
 del YonaDB.*
  
+call gradlew %1 build :dbinit:run
+if ERRORLEVEL 1 goto end
+
 start "HSQL database" java -cp "%HSQLDB_HOME%/lib/sqltool.jar" org.hsqldb.Server -database.0 file:YonaDB -dbname.0 xdb
-call gradlew %1 :dbinit:run
 start "Admin service" cmd /c gradlew %1 :adminservice:run
 start "Analysis service" cmd /c gradlew %1 :analysisservice:run
 start "App service" cmd /c gradlew %1 :appservice:run
@@ -15,3 +17,4 @@ echo Wait until all services are started.
 pause
 cmd /c gradlew --rerun-tasks :adminservice:intTest :analysisservice:intTest :appservice:intTest 
 
+:end
