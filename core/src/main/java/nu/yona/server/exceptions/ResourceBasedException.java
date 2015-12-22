dda5c72fa8a39d46c34383ab2c25c5535e84fc48
Bert Roos <bert@famroos.nu>
2015-12-22 06:07:33 +0100

    YD-138 ResourceBasedException now returns the translated message text

--- core/src/main/java/nu/yona/server/exceptions/ResourceBasedException.java
+++ core/src/main/java/nu/yona/server/exceptions/ResourceBasedException.java
@@ -1,14 +1,36 @@
 package nu.yona.server.exceptions;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import nu.yona.server.Translator;
 
 /**
  * This exception class is used as a base for all exceptions that use an error message which is defined in the message properties
- * 
- * @author pgussow
  */
 public abstract class ResourceBasedException extends RuntimeException
 {
+	@Component
+	private static class TranslatedTextRetriever
+	{
+		@Autowired
+		private Translator translator;
+
+		@EventListener({ ContextRefreshedEvent.class })
+		void onContextStarted(ContextRefreshedEvent event)
+		{
+			ResourceBasedException.setTranslatedTextRetriever(this);
+		}
+
+		String getLocalizedText(String messageId, Object... parameters)
+		{
+			return translator.getLocalizedMessage(messageId, parameters);
+		}
+	}
+
 	private static final long serialVersionUID = 8031973645020363969L;
 	/** Holds the parameters for the exception message. */
 	private final Object[] parameters;
@@ -16,6 +38,7 @@ public abstract class ResourceBasedException extends RuntimeException
 	private final String messageId;
 	/** Holds the HTTP response code to be used. */
 	private final HttpStatus statusCode;
+	private static TranslatedTextRetriever translatedTextRetriever;
 
 	/**
 	 * Constructor.
@@ -31,6 +54,11 @@ public abstract class ResourceBasedException extends RuntimeException
 		this.messageId = messageId;
 		this.parameters = parameters;
 		this.statusCode = statusCode;
+	}
+
+	public static void setTranslatedTextRetriever(TranslatedTextRetriever translatedTextRetriever)
+	{
+		ResourceBasedException.translatedTextRetriever = translatedTextRetriever;
 	}
 
 	/**
@@ -73,6 +101,23 @@ public abstract class ResourceBasedException extends RuntimeException
 		this.statusCode = statusCode;
 	}
 
+	@Override
+	public String getMessage()
+	{
+		return getLocalizedMessage();
+	}
+
+	@Override
+	public String getLocalizedMessage()
+	{
+		String localizedMessage = tryTranslateMessage();
+		if (localizedMessage == null)
+		{
+			localizedMessage = formAlternativeMessageText();
+		}
+		return localizedMessage;
+	}
+
 	/**
 	 * This method gets the message id.
 	 * 
@@ -101,5 +146,43 @@ public abstract class ResourceBasedException extends RuntimeException
 	public HttpStatus getStatusCode()
 	{
 		return statusCode;
+	}
+
+	private String tryTranslateMessage()
+	{
+		if (translatedTextRetriever == null)
+		{
+			return null;
+		}
+		try
+		{
+			return translatedTextRetriever.getLocalizedText(messageId, parameters);
+		}
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+
+	private String formAlternativeMessageText()
+	{
+		StringBuffer sb = new StringBuffer(messageId);
+		if ((parameters != null) && (parameters.length != 0))
+		{
+			sb.append("; parameters: ");
+		}
+		boolean isFirst = true;
+		for (Object parameter : parameters)
+		{
+			if (!isFirst)
+			{
+				sb.append(", ");
+			}
+			isFirst = false;
+			sb.append('"');
+			sb.append((parameter == null) ? "null" : parameter.toString());
+			sb.append('"');
+		}
+		return sb.toString();
 	}
 }
---
+++ core/src/test/java/nu/yona/server/exceptions/ResourceBasedExceptionTest.java
@@ -0,0 +1,73 @@
+package nu.yona.server.exceptions;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Configuration
+@ComponentScan(basePackages = { "nu.yona.server" })
+class MainContext
+{
+}
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { MainContext.class })
+public class ResourceBasedExceptionTest
+{
+
+	private static class TestException extends ResourceBasedException
+	{
+		protected TestException(String messageId, Object... parameters)
+		{
+			super(messageId, parameters);
+		}
+
+		private static final long serialVersionUID = 1L;
+
+	}
+
+	@Test
+	public void testSuccessfulMessageTranslationWithoutInsertions()
+	{
+		String messageId = "error.invalid.request";
+		String expectedResult = "Invalid request";
+		assertExceptionTranslation(expectedResult, messageId);
+	}
+
+	@Test
+	public void testFailedMessageTranslationWithoutInsertions()
+	{
+		String messageID = "non.existing.message.id";
+		String expectedResult = messageID;
+		assertExceptionTranslation(expectedResult, messageID);
+	}
+
+	@Test
+	public void testSuccessfulMessageTranslationWithInsertions()
+	{
+		String messageId = "error.sms.sending.failed.httpStatus";
+		String expectedResult = "Unexpected status code received from SMS service: first. Message: second";
+		assertExceptionTranslation(expectedResult, messageId, "first", "second");
+	}
+
+	@Test
+	public void testFailedMessageTranslationWithInsertions()
+	{
+		String messageID = "non.existing.message.id";
+		String expectedResult = messageID + "; parameters: \"first\", \"second\"";
+		assertExceptionTranslation(expectedResult, messageID, "first", "second");
+	}
+
+	private void assertExceptionTranslation(String expectedResult, String messageId, Object... parameters)
+	{
+		TestException exception = new TestException(messageId, parameters);
+		assertThat(exception.getMessage(), equalTo(expectedResult));
+		assertThat(exception.getLocalizedMessage(), equalTo(expectedResult));
+	}
+}
