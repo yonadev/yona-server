b3a22d9011de8b22634bdae890ae0f67b78d7414
aristotelos <arisvd@gmail.com>
2016-02-08 22:44:17 +0100

    YD-165 Fixed broken test!

--- analysisservice/src/intTest/groovy/nu/yona/server/RelevantSmoothwallCategoriesTest.groovy
+++ analysisservice/src/intTest/groovy/nu/yona/server/RelevantSmoothwallCategoriesTest.groovy
@@ -25,9 +25,10 @@ class RelevantSmoothwallCategoriesTest extends Specification
 
 		then:
 		response.status == 200
-		response.responseData.categories.size() == 4
+		response.responseData.categories.size() == 5
 		response.responseData.categories.contains("Gambling")
 		response.responseData.categories.contains("lotto")
+		response.responseData.categories.contains("social")
 		response.responseData.categories.contains("news/media")
 		response.responseData.categories.contains("newsgroups/forums")
 	}
