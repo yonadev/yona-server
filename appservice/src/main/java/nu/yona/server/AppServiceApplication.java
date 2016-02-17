5f94c7666eddf9214de83569dc08935e0d5d6dcb
Bert Roos <bert@famroos.nu>
2016-02-17 21:41:22 +0100

    YD-184 Removed SpringFox
    as it was causing an issue with the repro

--- appservice/src/main/java/nu/yona/server/AppServiceApplication.java
+++ appservice/src/main/java/nu/yona/server/AppServiceApplication.java
@@ -4,41 +4,16 @@
  *******************************************************************************/
 package nu.yona.server;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableCaching
 public class AppServiceApplication
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(AppServiceApplication.class, args);
-	}
-
-	@Bean
-	public Docket yonaApi()
-	{
-		ApiInfo apiInfo = new ApiInfo("Yona Server", "Server backing the Yona mobile app", "1.0", null, null, "MPL",
-				"https://www.mozilla.org/en-US/MPL/2.0/");
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build().pathMapping("/").useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
-						.message("500 message").responseModel(new ModelRef("Error")).build()));
 	}
 }
