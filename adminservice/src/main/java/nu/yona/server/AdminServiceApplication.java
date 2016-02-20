580d5fa929b4d0f7ea4f504ff4f915db352ab263
Bert Roos <bert@famroos.nu>
2016-02-20 17:28:59 +0100

    YD-175 Removed SpringFox
    We decided to go for a handcrafted Swagger spec, as SpringFox was too cumbersome to describe the entire API.

--- adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
+++ adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
@@ -4,61 +4,18 @@
  *******************************************************************************/
 package nu.yona.server;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static springfox.documentation.schema.AlternateTypeRules.newRule;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.fasterxml.classmate.TypeResolver;
-
-import nu.yona.server.goals.rest.ActivityCategoryController.ActivityCategoryResource;
-import nu.yona.server.goals.service.ActivityCategoryDTO;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.AlternateTypeRule;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan("nu.yona.server")
 @SpringBootApplication
-@EnableSwagger2
 @EnableCaching
 public class AdminServiceApplication
 {
-	@Autowired
-	private TypeResolver typeResolver;
-
 	public static void main(String[] args)
 	{
 		SpringApplication.run(AdminServiceApplication.class, args);
-	}
-
-	@Bean
-	public Docket yonaApi()
-	{
-		ApiInfo apiInfo = new ApiInfo("Yona Administration", "Administrative APIs of Yona server", "1.0", null, null, "MPL",
-				"https://www.mozilla.org/en-US/MPL/2.0/");
-		AlternateTypeRule activityCategoryResourcesRule = newRule(
-				typeResolver.resolve(HttpEntity.class, typeResolver.resolve(Resources.class, ActivityCategoryResource.class)),
-				typeResolver.resolve(List.class, ActivityCategoryDTO.class));
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build().pathMapping("/").alternateTypeRules(activityCategoryResourcesRule)
-				.useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
-						.message("500 message").responseModel(new ModelRef("Error")).build()));
 	}
 }
