6a24cac3b79ef8045d652c2327c2658432916a29
Bert Roos <bert@famroos.nu>
2016-03-21 18:42:46 +0100

    Updated/created the copyright headers

--- core/src/main/java/nu/yona/server/CoreConfiguration.java
+++ core/src/main/java/nu/yona/server/CoreConfiguration.java
@@ -1,6 +1,6 @@
 package nu.yona.server;
 /*******************************************************************************
- * Copyright (c) 2015 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
+ * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
  * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 
