b3f5da8127cbf77b9caec1ab9933cae26b3ca92f
aristotelos <arisvd@gmail.com>
2016-04-07 21:06:45 +0200

    YD-208 Refactored some lines, added first methods

--- core/src/main/java/nu/yona/server/analysis/service/ActivityService.java
+++ core/src/main/java/nu/yona/server/analysis/service/ActivityService.java
@@ -2,6 +2,8 @@ package nu.yona.server.analysis.service;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 import nu.yona.server.analysis.entities.DayActivity;
 import nu.yona.server.analysis.entities.WeekActivity;
 import nu.yona.server.analysis.service.IntervalActivityDTO.LevelOfDetail;
+import nu.yona.server.goals.entities.Goal;
 import nu.yona.server.properties.YonaProperties;
 import nu.yona.server.subscriptions.service.UserAnonymizedDTO;
 import nu.yona.server.subscriptions.service.UserAnonymizedService;
@@ -37,9 +40,9 @@ public class ActivityService
 	{
 		UUID userAnonymizedID = userService.getPrivateUser(userID).getPrivateData().getUserAnonymizedID();
 		UserAnonymizedDTO userAnonymized = userAnonymizedService.getUserAnonymized(userAnonymizedID);
-		LocalDate dateFrom = LocalDate.now(ZoneId.of(userAnonymized.getTimeZoneId())).minusWeeks(pageable.getOffset());
-		LocalDate dateUntil = dateFrom.minusWeeks(pageable.getPageSize());
-		Set<WeekActivity> weekActivityEntities = WeekActivity.getRepository().findAll(userAnonymizedID, dateUntil, dateFrom);
+		StartEndDate startEndDate = getStartEndDate(getCurrentWeekDate(userAnonymized), pageable, ChronoUnit.WEEKS);
+		Set<WeekActivity> weekActivityEntities = WeekActivity.getRepository().findAll(userAnonymizedID, startEndDate.startDate,
+				startEndDate.endDate);
 		Map<LocalDate, Set<WeekActivity>> weekActivityEntitiesByDate = weekActivityEntities.stream()
 				.collect(Collectors.groupingBy(a -> a.getDate(), Collectors.toSet()));
 		return new PageImpl<WeekActivityOverviewDTO>(weekActivityEntitiesByDate.entrySet().stream()
@@ -51,14 +54,81 @@ public class ActivityService
 	{
 		UUID userAnonymizedID = userService.getPrivateUser(userID).getPrivateData().getUserAnonymizedID();
 		UserAnonymizedDTO userAnonymized = userAnonymizedService.getUserAnonymized(userAnonymizedID);
-		LocalDate dateFrom = LocalDate.now(ZoneId.of(userAnonymized.getTimeZoneId())).minusDays(pageable.getOffset());
-		LocalDate dateUntil = dateFrom.minusDays(pageable.getPageSize());
-		Set<DayActivity> datActivityEntities = DayActivity.getRepository().findAll(userAnonymizedID, dateUntil, dateFrom);
-		Map<LocalDate, Set<DayActivity>> dayActivityEntitiesByDate = datActivityEntities.stream()
+		StartEndDate startEndDate = getStartEndDate(getCurrentDayDate(userAnonymized), pageable, ChronoUnit.DAYS);
+		Set<DayActivity> dayActivityEntities = DayActivity.getRepository().findAll(userAnonymizedID, startEndDate.startDate,
+				startEndDate.endDate);
+		Map<LocalDate, Set<DayActivity>> dayActivityEntitiesByDate = dayActivityEntities.stream()
 				.collect(Collectors.groupingBy(a -> a.getDate(), Collectors.toSet()));
+		addMissingZeroActivity(dayActivityEntitiesByDate, startEndDate, ChronoUnit.DAYS, userAnonymized);
 		return new PageImpl<DayActivityOverviewDTO>(dayActivityEntitiesByDate.entrySet().stream()
 				.map(e -> DayActivityOverviewDTO.createInstance(e.getValue())).collect(Collectors.toList()), pageable,
 				yonaProperties.getAnalysisService().getDaysActivityMemory());
+	}
+
+	private StartEndDate getStartEndDate(LocalDate currentUnitDate, Pageable pageable, ChronoUnit timeUnit)
+	{
+		LocalDate dateFrom = currentUnitDate.minus(pageable.getOffset(), timeUnit);
+		LocalDate dateUntil = dateFrom.minus(pageable.getPageSize(), timeUnit);
+		return new StartEndDate(dateUntil, dateFrom);
+	}
+
+	private LocalDate getCurrentWeekDate(UserAnonymizedDTO userAnonymized)
+	{
+		LocalDate currentDayDate = getCurrentDayDate(userAnonymized);
+		switch (currentDayDate.getDayOfWeek())
+		{
+			case SUNDAY:
+				// take as the first day of week
+				return currentDayDate;
+			default:
+				// MONDAY=1, etc.
+				return currentDayDate.minusDays(currentDayDate.getDayOfWeek().getValue());
+		}
+	}
+
+	private LocalDate getCurrentDayDate(UserAnonymizedDTO userAnonymized)
+	{
+		return LocalDate.now(ZoneId.of(userAnonymized.getTimeZoneId()));
+	}
+
+	private void addMissingZeroActivity(Map<LocalDate, Set<DayActivity>> dayActivityEntitiesByDate, StartEndDate startEndDate,
+			ChronoUnit timeUnit, UserAnonymizedDTO userAnonymized)
+	{
+		Set<Goal> currentGoals = userAnonymized.getGoals();
+		for (LocalDate date = startEndDate.startDate; date.isBefore(startEndDate.endDate)
+				|| date.isEqual(startEndDate.endDate); date = date.plus(1, timeUnit))
+		{
+			if (!dayActivityEntitiesByDate.containsKey(date))
+			{
+				// TODO: check if any goals present (goal creation time)
+				dayActivityEntitiesByDate.put(date, new HashSet<DayActivity>());
+			}
+
+			Set<DayActivity> dayActivityEntitiesAtDate = dayActivityEntitiesByDate.get(date);
+			addMissingZeroActivity(currentGoals, date, dayActivityEntitiesAtDate, userAnonymized);
+		}
+	}
+
+	private void addMissingZeroActivity(Set<Goal> currentGoals, LocalDate date, Set<DayActivity> dayActivityEntitiesAtDate,
+			UserAnonymizedDTO userAnonymized)
+	{
+		currentGoals.forEach(g -> addMissingZeroActivity(g, date, dayActivityEntitiesAtDate, userAnonymized));
+	}
+
+	private void addMissingZeroActivity(Goal currentGoal, LocalDate date, Set<DayActivity> dayActivityEntitiesAtDate,
+			UserAnonymizedDTO userAnonymized)
+	{
+		// TODO: check goal creation time
+		if (!containsActivityForGoal(dayActivityEntitiesAtDate, currentGoal))
+		{
+			dayActivityEntitiesAtDate.add(
+					DayActivity.createInstance(null, currentGoal, date.atStartOfDay(ZoneId.of(userAnonymized.getTimeZoneId()))));
+		}
+	}
+
+	private boolean containsActivityForGoal(Set<DayActivity> dayActivityEntitiesAtDate, Goal goal)
+	{
+		return dayActivityEntitiesAtDate.stream().anyMatch(a -> a.getGoal().equals(goal));
 	}
 
 	public WeekActivityDTO getWeekActivityDetail(UUID userID, LocalDate date, UUID goalID)
@@ -73,5 +143,17 @@ public class ActivityService
 		UUID userAnonymizedID = userService.getPrivateUser(userID).getPrivateData().getUserAnonymizedID();
 		return DayActivityDTO.createInstance(DayActivity.getRepository().findOne(userAnonymizedID, date, goalID),
 				LevelOfDetail.DayDetail);
+	}
+
+	class StartEndDate
+	{
+		public final LocalDate startDate;
+		public final LocalDate endDate;
+
+		public StartEndDate(LocalDate startDate, LocalDate endDate)
+		{
+			this.startDate = startDate;
+			this.endDate = endDate;
+		}
 	}
 }
