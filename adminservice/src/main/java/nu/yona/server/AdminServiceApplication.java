373c8508ce0250829ae3a08792d5af14d91c3d2c
Bert Roos <bert@famroos.nu>
2016-04-30 15:22:50 +0200

    YD-220 Added Spring batch admin

--- adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
+++ adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
@@ -1,21 +1,44 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import de.codecentric.batch.SpringBatchAdmin;
+import de.codecentric.batch.config.MainConfiguration;
 
 @ComponentScan("nu.yona.server")
-@SpringBootApplication
 @EnableCaching
-public class AdminServiceApplication
+@Configuration
+@EnableAutoConfiguration(exclude = { BatchAutoConfiguration.class, DataSourceAutoConfiguration.class,
+		WebMvcAutoConfiguration.class })
+@Import(MainConfiguration.class)
+public class AdminServiceApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+	/**
+	 * @see org.springframework.boot.context.web.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(SpringBatchAdmin.class);
 	}
 }
