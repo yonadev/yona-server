48761ea635999e9c20ae708183e59f6f805d3897
Bert Roos <bert@famroos.nu>
2016-05-09 21:30:33 +0200

    YD-220 Simplified the configuration based on review comments.

--- adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
+++ adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
@@ -5,39 +5,24 @@
 package nu.yona.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import nu.yona.server.admin.batch.SpringBatchAdmin;
 import nu.yona.server.admin.batch.config.MainConfiguration;
 
-@ComponentScan("nu.yona.server")
+@SpringBootApplication(scanBasePackages = { "nu.yona.server" }, exclude = { BatchAutoConfiguration.class,
+		DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @EnableCaching
-@Configuration
-@EnableAutoConfiguration(exclude = { BatchAutoConfiguration.class, DataSourceAutoConfiguration.class,
-		WebMvcAutoConfiguration.class })
 @Import(MainConfiguration.class)
 public class AdminServiceApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(AdminServiceApplication.class, args);
-	}
-
-	/**
-	 * @see org.springframework.boot.context.web.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
-	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-	{
-		return application.sources(SpringBatchAdmin.class);
 	}
 }
