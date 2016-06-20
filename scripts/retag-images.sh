3c1d44f6a1d10adedf85127679b49272e6111c47
Bert Roos <bert@famroos.nu>
2016-06-20 21:59:46 +0200

    Enabled retaining builds delivered to Mobiquity
    * The docker-compose.yml now contains a variable name yonatag for the tags. This variable must be set before calling docker-compose
    * Added a script to retag the build with a build number
    * Added a script to refresh the build to a given build number

---
+++ scripts/retag-images.sh
@@ -0,0 +1,10 @@
+docker tag yonadev/appservice:0.0.8-SNAPSHOT yonadev/appservice:build-$1
+docker tag yonadev/adminservice:0.0.8-SNAPSHOT yonadev/adminservice:build-$1
+docker tag yonadev/batchservice:0.0.8-SNAPSHOT yonadev/batchservice:build-$1
+docker tag yonadev/analysisservice:0.0.8-SNAPSHOT yonadev/analysisservice:build-$1
+docker tag yonadev/yonahsqldb:0.0.8-SNAPSHOT yonadev/yonahsqldb:build-$1
+docker push yonadev/appservice:build-$1
+docker push yonadev/adminservice:build-$1
+docker push yonadev/batchservice:build-$1
+docker push yonadev/analysisservice:build-$1
+docker push yonadev/yonahsqldb:build-$1
