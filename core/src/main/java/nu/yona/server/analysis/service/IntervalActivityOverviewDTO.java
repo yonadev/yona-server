531bc41b524d36aee68c91bcfed2ee688a3a0a21
Bert-R <bert@famroos.nu>
2016-07-06 08:05:11 +0200

    YD-292 Brought the "date" property back

--- core/src/main/java/nu/yona/server/analysis/service/IntervalActivityOverviewDTO.java
+++ core/src/main/java/nu/yona/server/analysis/service/IntervalActivityOverviewDTO.java
@@ -7,6 +7,8 @@ package nu.yona.server.analysis.service;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public abstract class IntervalActivityOverviewDTO
 {
 	private final ZonedDateTime date;
@@ -19,6 +21,7 @@ public abstract class IntervalActivityOverviewDTO
 	/*
 	 * The ISO-8601 week or day date.
 	 */
+	@JsonProperty("date")
 	public String getDateStr()
 	{
 		return formatDateAsISO(date.toLocalDate());
