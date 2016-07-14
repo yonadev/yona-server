d3668ac1a37695b604c09c85793d751b8be14bc7
aristotelos <arisvd@gmail.com>
2016-07-14 20:22:29 +0200

    YD-303 Removed embedded profile from DTO, moved to static file

--- core/src/main/java/nu/yona/server/subscriptions/service/VPNProfileDTO.java
+++ core/src/main/java/nu/yona/server/subscriptions/service/VPNProfileDTO.java
@@ -1,21 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.subscriptions.service;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.google.common.io.Resources;
 
-import nu.yona.server.exceptions.YonaException;
 import nu.yona.server.subscriptions.entities.User;
 
 @JsonRootName("vpnProfile")
@@ -58,19 +53,6 @@ public class VPNProfileDTO
 	public void setVpnPassword(String vpnPassword)
 	{
 		this.vpnPassword = vpnPassword;
-	}
-
-	public String getOpenVPNProfile()
-	{
-		try
-		{
-			URL url = Resources.getResource("OpenVPNProfile.ovpn");
-			return Resources.toString(url, StandardCharsets.UTF_8).replace("\n", "\\n");
-		}
-		catch (IOException e)
-		{
-			throw YonaException.unexpected(e);
-		}
 	}
 
 	public static VPNProfileDTO createInstance(User user)
