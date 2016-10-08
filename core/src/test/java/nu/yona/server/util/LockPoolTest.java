432a3ab77f59603bb58e8c83b9ccd9ca9b329bf4
Bert-R <bert@famroos.nu>
2016-10-08 11:30:34 +0200

    YD-370 Processed review comments

--- core/src/test/java/nu/yona/server/util/LockPoolTest.java
+++ core/src/test/java/nu/yona/server/util/LockPoolTest.java
@@ -21,16 +21,16 @@ public class LockPoolTest
 	public void testLockingNonReentrant()
 	{
 		final int numThreads = 10;
-		final int iterations = 10000;
-		assertThat(testConcurrently(numThreads, iterations, this::attemptNonReentrantAccess), equalTo(0));
+		final int iterations = 100;
+		assertThat(testConcurrently(numThreads, iterations, this::attemptNonReentrantAccess), equalTo(false));
 	}
 
 	@Test
 	public void testLockingReentrant()
 	{
 		final int numThreads = 10;
-		final int iterations = 10000;
-		assertThat(testConcurrently(numThreads, iterations, this::attemptReentrantAccess), equalTo(0));
+		final int iterations = 100;
+		assertThat(testConcurrently(numThreads, iterations, this::attemptReentrantAccess), equalTo(false));
 	}
 
 	@Test
@@ -38,67 +38,67 @@ public class LockPoolTest
 	{
 		final int numThreads = 10;
 		final int iterations = 5;
-		assertThat(testConcurrently(numThreads, iterations, this::attempImmediateAccessRandomID), equalTo(0));
+		assertThat(testConcurrently(numThreads, iterations, this::attempImmediateAccessRandomID), equalTo(false));
 	}
 
-	private int testConcurrently(int numThreads, int iterations, AccessAttempt accessAttempt)
+	private boolean testConcurrently(int numThreads, int iterations, AccessAttempt accessAttempt)
 	{
 		LockPool<Integer> lockPool = new LockPool<>();
 		ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
 		CountDownLatch startSignal = new CountDownLatch(1);
 		CountDownLatch doneSignal = new CountDownLatch(numThreads);
 		AtomicBoolean concurrencyIndicator = new AtomicBoolean(false);
-		AtomicInteger failureCounter = new AtomicInteger(0);
+		AtomicBoolean failureIndicator = new AtomicBoolean(false);
 		for (int i = 0; (i < numThreads); i++)
 		{
-			threadPool.execute(() -> tryAccess(iterations, startSignal, lockPool, concurrencyIndicator, failureCounter,
+			threadPool.execute(() -> tryAccess(iterations, startSignal, lockPool, concurrencyIndicator, failureIndicator,
 					doneSignal, accessAttempt));
 		}
 		startSignal.countDown();
 		awaitWithoutInterrupt(doneSignal);
 		threadPool.shutdown();
-		return failureCounter.get();
+		return failureIndicator.get();
 	}
 
 	private void tryAccess(int iterations, CountDownLatch startSignal, LockPool<Integer> lockPool,
-			AtomicBoolean concurrencyIndicator, AtomicInteger failureCounter, CountDownLatch doneSignal,
-			AccessAttempt accessAtempt)
+			AtomicBoolean concurrencyIndicator, AtomicBoolean failureIndicator, CountDownLatch doneSignal,
+			AccessAttempt accessAttempt)
 	{
 		awaitWithoutInterrupt(startSignal);
 		for (int i = 0; (i < iterations); i++)
 		{
-			accessAtempt.attempt(lockPool, concurrencyIndicator, failureCounter);
+			accessAttempt.attempt(lockPool, concurrencyIndicator, failureIndicator);
 		}
 		doneSignal.countDown();
 	}
 
 	private void attemptNonReentrantAccess(LockPool<Integer> lockPool, AtomicBoolean concurrencyIndicator,
-			AtomicInteger failureCounter)
+			AtomicBoolean failureIndicator)
 	{
 		int lockID = 0;
 		try (LockPool<Integer>.Lock lock = lockPool.lock(lockID))
 		{
-			verifyNonConcurrentExecution(concurrencyIndicator, failureCounter);
+			verifyNonConcurrentExecution(concurrencyIndicator, failureIndicator);
 		}
 	}
 
 	private void attemptReentrantAccess(LockPool<Integer> lockPool, AtomicBoolean concurrencyIndicator,
-			AtomicInteger failureCounter)
+			AtomicBoolean failureIndicator)
 	{
 		int lockID = 0;
-		try (LockPool<Integer>.Lock lock1 = lockPool.lock(0))
+		try (LockPool<Integer>.Lock lock1 = lockPool.lock(lockID))
 		{
-			verifyNonConcurrentExecution(concurrencyIndicator, failureCounter);
+			verifyNonConcurrentExecution(concurrencyIndicator, failureIndicator);
 			try (LockPool<Integer>.Lock lock2 = lockPool.lock(lockID))
 			{
-				verifyNonConcurrentExecution(concurrencyIndicator, failureCounter);
+				verifyNonConcurrentExecution(concurrencyIndicator, failureIndicator);
 			}
-			verifyNonConcurrentExecution(concurrencyIndicator, failureCounter);
+			verifyNonConcurrentExecution(concurrencyIndicator, failureIndicator);
 		}
 	}
 
 	private void attempImmediateAccessRandomID(LockPool<Integer> lockPool, AtomicBoolean concurrencyIndicator,
-			AtomicInteger failureCounter)
+			AtomicBoolean failureIndicator)
 	{
 		int lockID = freeLockID.incrementAndGet();
 		int sleepDuration = 500;
@@ -115,21 +115,24 @@ public class LockPoolTest
 		if (accessTime - startTime > sleepDuration / 2)
 		{
 			// Delay was more than half the sleep duration, so we apparently were held up
-			failureCounter.incrementAndGet();
+			// Comparing to the exact sleep duration is tricky, as the contract for Thread.sleep mentions accuracy of the system
+			// timers.
+			failureIndicator.set(true);
 		}
 	}
 
-	private void verifyNonConcurrentExecution(AtomicBoolean concurrencyIndicator, AtomicInteger failureCounter)
+	private void verifyNonConcurrentExecution(AtomicBoolean concurrencyIndicator, AtomicBoolean failureIndicator)
 	{
 		if (concurrencyIndicator.compareAndSet(false, true))
 		{
+			sleepWithoutInterrupt(1);
 			// No one else is here
 			concurrencyIndicator.set(false);
 		}
 		else
 		{
 			// Someone else is here
-			failureCounter.incrementAndGet();
+			failureIndicator.set(true);
 		}
 	}
 
@@ -160,6 +163,6 @@ public class LockPoolTest
 	@FunctionalInterface
 	static interface AccessAttempt
 	{
-		void attempt(LockPool<Integer> lockPool, AtomicBoolean concurrencyIndicator, AtomicInteger testCounter);
+		void attempt(LockPool<Integer> lockPool, AtomicBoolean concurrencyIndicator, AtomicBoolean failureIndicator);
 	}
 }
