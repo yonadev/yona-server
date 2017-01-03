85538fce8bfba5396d2e1754f57711e06ef5db1a
aristotelos <arisvd@gmail.com>
2017-01-03 22:08:29 +0100

    YD-319 Rephrased comment

--- analysisservice/src/test/java/nu/yona/server/analysis/service/ActivityServiceTests.java
+++ analysisservice/src/test/java/nu/yona/server/analysis/service/ActivityServiceTests.java
@@ -377,7 +377,8 @@ public class ActivityServiceTests
 		int hour = 20;
 		int[] expectedSpread = getEmptySpread();
 		expectedSpread[hour * 4] = 1;
-		// An activity duration of less than 1 minute is never logged. See AnalysisEngineService.ensureMinimumDurationOneMinute.
+		// An activity duration of less than 1 minute is always upgraded to 1 minute See
+		// AnalysisEngineService.ensureMinimumDurationOneMinute.
 		assertSpread("20:05:08", "20:06:11", expectedSpread);
 	}
 
