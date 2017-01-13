fd4904cfd4bd62a1e6294ad4ba4258ea46042dad
aristotelos <arisvd@gmail.com>
2017-01-13 17:51:36 +0100

    YD-400 Phone number whitelist (#291)
    * YD-400 Initial setup
    * YD-400 Simplified code, added validation
    * YD-400 Added property, default false
    * YD-400 Using redirect
    * YD-400 Added message translation
    * YD-400 Removed caching, removed unused code
    * YD-400 Rework from review
      Changed whitelist/whitelisted to white list/white-listed
      Using maxUsers property
      Using findByMobileNumber
      Small renames

---
+++ core/src/main/java/nu/yona/server/subscriptions/service/WhitelistedNumberServiceException.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *******************************************************************************/
+package nu.yona.server.subscriptions.service;
+
+import org.springframework.http.HttpStatus;
+
+import nu.yona.server.exceptions.YonaException;
+
+public class WhiteListedNumberServiceException extends YonaException
+{
+	private static final long serialVersionUID = 2543728671534987461L;
+
+	private WhiteListedNumberServiceException(String messageId, Object... parameters)
+	{
+		super(messageId, parameters);
+	}
+
+	private WhiteListedNumberServiceException(HttpStatus statusCode, String messageId, Object... parameters)
+	{
+		super(statusCode, messageId, parameters);
+	}
+
+	public static WhiteListedNumberServiceException numberNotWhiteListed(String mobileNumber)
+	{
+		return new WhiteListedNumberServiceException(HttpStatus.BAD_REQUEST, "error.number.not.white.listed", mobileNumber);
+	}
+}
