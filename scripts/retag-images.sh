af4bbcb6c9657442883ed5f4698b147a10ae017e
Bert-R <bert@famroos.nu>
2017-01-17 21:20:53 +0100

    Improvements based on first deployment
    Deploying the MariaDB-based version revealed some issues:
    * refresh-build.sh and retag-images.sh needed to be updated for container changes and database schema updates
    * docker-compose.yml was using volumes the wrong way
    * Property names for yona.ldap.baseDn and yona.ldap.accessUserDn were wrong (ending with N instead of n)

--- scripts/retag-images.sh
+++ scripts/retag-images.sh
@@ -1,8 +1,10 @@
-docker tag yonadev/appservice:0.0.8-SNAPSHOT yonadev/appservice:build-$1
-docker tag yonadev/adminservice:0.0.8-SNAPSHOT yonadev/adminservice:build-$1
-docker tag yonadev/batchservice:0.0.8-SNAPSHOT yonadev/batchservice:build-$1
-docker tag yonadev/analysisservice:0.0.8-SNAPSHOT yonadev/analysisservice:build-$1
+docker tag yonadev/appservice:latest yonadev/appservice:build-$1
+docker tag yonadev/adminservice:latest yonadev/adminservice:build-$1
+docker tag yonadev/batchservice:latest yonadev/batchservice:build-$1
+docker tag yonadev/analysisservice:latest yonadev/analysisservice:build-$1
+docker tag yonadev/yona-mariadb-liquibase-update:latest yonadev/yona-mariadb-liquibase-update:build-$1
 docker push yonadev/appservice:build-$1
 docker push yonadev/adminservice:build-$1
 docker push yonadev/batchservice:build-$1
 docker push yonadev/analysisservice:build-$1
+docker push yonadev/yona-mariadb-liquibase-update:build-$1
