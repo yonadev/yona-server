208b7a59165cfd298db40d7e94307bd7832a750b
Bert-R <bert@famroos.nu>
2017-01-24 19:44:48 +0100

    Updated copyrights

--- core/src/main/java/nu/yona/server/email/EmailService.java
+++ core/src/main/java/nu/yona/server/email/EmailService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
+ * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
  * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.email;
