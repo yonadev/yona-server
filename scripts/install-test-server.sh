23d052a7fd2b38c441689fb832ec734595293933
Bert-R <bert@famroos.nu>
2017-02-17 19:40:11 +0100

    Script changes
    * Improved retag-images.sh by obsoleting the build number parameter. The latest build number is now fetched automatically.
    * Added the test server install script to Git, so we can trigger it from Jenkins

---
+++ scripts/install-test-server.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -e # Fail on error
+
+function waitTillGetWorks() {
+	n=0
+	until [ $n -ge 12 ]
+	do
+		curl -f $1 && echo && break
+		n=$[$n+1]
+		sleep 5
+	done
+	[ $n -ge 12 ] && echo Failed to get URL $1 within timeout
+}
+
+# Go to the home of the root user
+cd /root/docker-compose/yona
+
+export yonatag=latest
+
+echo "Stopping all Yona Docker containers"
+docker-compose stop
+
+# Go to the MariaDB directory
+pushd ../mariadb
+
+echo "Stopping MariaDB"
+docker-compose stop
+
+echo "Removing the old database"
+docker-compose rm -f -v
+
+echo "Updating the MariaDB docker-compose.yml file"
+wget -O docker-compose.yml https://raw.githubusercontent.com/yonadev/yona-server/master/docker-compose/mariadb/docker-compose.yml
+
+echo "Pulling latest MariaDB image"
+docker-compose pull
+
+echo "Starting MariaDB"
+docker-compose up -d
+
+echo "Waiting for MariaDB"
+docker run --rm --network yonanet --link mariadb:mariadb -e TARGETS=mariadb:3306 waisbrot/wait
+
+echo "Loading the Yona database schema"
+docker run --rm -i --network yonanet --link mariadb:yonadbserver -e USER=root -e PASSWORD=root -e URL=jdbc:mariadb://yonadbserver:3306/yona  yonadev/yona-mariadb-liquibase-update:latest
+
+# Return to Yona Docker compose folder
+popd
+
+echo "Removing the Yona Docker containers"
+docker-compose rm -f -v
+
+echo "Updating the docker-compose.yml file"
+wget -O docker-compose.yml https://raw.githubusercontent.com/yonadev/yona-server/master/docker-compose/yona/docker-compose.yml
+
+echo "Pulling all images"
+docker-compose pull
+
+echo "Generating database connection environment file"
+cat << EOF > db_settings.env
+YONA_DB_USER_NAME=$1
+YONA_DB_PASSWORD=$2
+YONA_DB_URL=$3
+EOF
+
+echo "Starting the containers"
+docker-compose up -d
+
+echo "Waiting for the admin service to start"
+waitTillGetWorks http://localhost:8080/activityCategories/
+
+echo "Waiting for the analysis service to start"
+waitTillGetWorks http://localhost:8081/relevantSmoothwallCategories/
+
+echo "Waiting for the app service to start"
+waitTillGetWorks http://localhost/activityCategories/
+
+echo "Loading the activity categories"
+curl https://raw.githubusercontent.com/yonadev/yona-server/master/dbinit/data/activityCategories.json | curl -X PUT http://localhost:8080/activityCategories/ -d @- --header "Content-Type: application/json"
--- scripts/retag-images.sh
+++ scripts/retag-images.sh
@@ -1,10 +1,14 @@
-docker tag yonadev/appservice:latest yonadev/appservice:build-$1
-docker tag yonadev/adminservice:latest yonadev/adminservice:build-$1
-docker tag yonadev/batchservice:latest yonadev/batchservice:build-$1
-docker tag yonadev/analysisservice:latest yonadev/analysisservice:build-$1
-docker tag yonadev/yona-mariadb-liquibase-update:latest yonadev/yona-mariadb-liquibase-update:build-$1
-docker push yonadev/appservice:build-$1
-docker push yonadev/adminservice:build-$1
-docker push yonadev/batchservice:build-$1
-docker push yonadev/analysisservice:build-$1
-docker push yonadev/yona-mariadb-liquibase-update:build-$1
+#!/bin/bash
+set -e # Fail on error
+
+export jenkinsUrl="https://yonadev.ci.cloudbees.com/job/build-yonadev-master/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/number/text()"
+#Extra curl call to ensure we fail if curl fails
+curl -f -s -H "Accept:application/json" "$jenkinsUrl"
+
+export build=`curl -f -s -H "Accept:application/json" "$jenkinsUrl"`
+for image in appservice adminservice batchservice analysisservice yona-mariadb-liquibase-update
+do
+	echo Tagging and pushing yonadev/$image:build-$build
+	docker tag yonadev/$image:latest yonadev/$image:build-$build
+	docker push yonadev/$image:build-$build
+done
