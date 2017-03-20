5b893765ad1ca04273f0b4a96cd5b31400da6d1c
aristotelos <arisvd@gmail.com>
2017-03-20 21:43:07 +0100

    YD-265 Removed unused method, updated comment

--- core/src/main/java/nu/yona/server/subscriptions/service/UserService.java
+++ core/src/main/java/nu/yona/server/subscriptions/service/UserService.java
@@ -167,16 +167,6 @@ public class UserService
 	}
 
 	@Transactional
-	public void clearOverwriteUserConfirmationCode(String mobileNumber)
-	{
-		User existingUserEntity = findUserByMobileNumber(mobileNumber);
-		existingUserEntity.setOverwriteUserConfirmationCode(null);
-		userRepository.save(existingUserEntity);
-		logger.info("User with mobile number '{}' and ID '{}' cleared the account overwrite confirmation code",
-				existingUserEntity.getMobileNumber(), existingUserEntity.getId());
-	}
-
-	@Transactional
 	public UserDto addUser(UserDto user, Optional<String> overwriteUserConfirmationCode)
 	{
 		assertValidUserFields(user, UserPurpose.USER);
