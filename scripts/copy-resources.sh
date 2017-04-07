7dfa18b2f648b85d5e4d8b856911e828a9cd20d4
Bert-R <bert@famroos.nu>
2017-04-07 19:32:38 +0200

    One more correction

--- scripts/copy-resources.sh
+++ scripts/copy-resources.sh
@@ -1,15 +1,15 @@
-export yonatag=build-$1
+export github_version=$1
 export resources_source_folder=$2
 
 [[ -d resources ]] || mkdir resources
 
 pushd resources
 echo "Copying base resources from GitHub"
-wget https://raw.githubusercontent.com/yonadev/yona-server/$yonatag/resources/apple.mobileconfig.xml
-wget https://raw.githubusercontent.com/yonadev/yona-server/$yonatag/resources/profile.ovpn
-wget https://raw.githubusercontent.com/yonadev/yona-server/$yonatag/resources/rootcert.cer
-wget https://raw.githubusercontent.com/yonadev/yona-server/$yonatag/resources/smime.crt
-wget https://raw.githubusercontent.com/yonadev/yona-server/$yonatag/resources/smime.key
+wget https://raw.githubusercontent.com/yonadev/yona-server/$github_version/resources/apple.mobileconfig.xml
+wget https://raw.githubusercontent.com/yonadev/yona-server/$github_version/resources/profile.ovpn
+wget https://raw.githubusercontent.com/yonadev/yona-server/$github_version/resources/rootcert.cer
+wget https://raw.githubusercontent.com/yonadev/yona-server/$github_version/resources/smime.crt
+wget https://raw.githubusercontent.com/yonadev/yona-server/$github_version/resources/smime.key
 
 echo "Copying node-specific resources from $resources_source_folder"
 cp -r $resources_source_folder/* .
