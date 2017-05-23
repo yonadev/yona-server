8e16933de2e4292643e70f91c75b02a96385b890
Bert Roos <Bert-R@users.noreply.github.com>
2017-05-23 20:04:51 +0200

    YD-460 #done Switch signing on (#360)
    Though with an option to switch it off through a property

--- core/src/main/java/nu/yona/server/properties/AppleMobileConfigProperties.java
+++ core/src/main/java/nu/yona/server/properties/AppleMobileConfigProperties.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.properties;
 
 public class AppleMobileConfigProperties
 {
 	private String appleMobileConfigFile;
+	private boolean isSigningEnabled = true;
 	private String signingKeyStoreFile;
 	private String signingKeyStorePassword;
 	private String signingAlias;
@@ -19,6 +20,16 @@ public class AppleMobileConfigProperties
 	public void setAppleMobileConfigFile(String appleMobileConfigFile)
 	{
 		this.appleMobileConfigFile = appleMobileConfigFile;
+	}
+
+	public boolean isSigningEnabled()
+	{
+		return isSigningEnabled;
+	}
+
+	public void setSigningEnabled(boolean isSigningEnabled)
+	{
+		this.isSigningEnabled = isSigningEnabled;
 	}
 
 	public String getSigningKeyStoreFile()
