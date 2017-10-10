f494efea643753a4d06ff733a48eaf692262135d
Bert-R <bert@famroos.nu>
2017-10-10 07:29:00 +0200

    YD-489 Use @ConditionalOnProperty
    Originally, we used to return null from the bean initialization method for the LDAP template, but this was wrong. With Spring Boot 1.4.7, this was tolerated but with 1.5.7, it fails in Actuator. On spring-projects/spring-boot#10558, @philwebb pointed out the mistake and provided the right solution: use @ConditionalOnProperty("yona.ldap.enabled")

--- core/src/main/java/nu/yona/server/subscriptions/service/LDAPUserService.java
+++ core/src/main/java/nu/yona/server/subscriptions/service/LDAPUserService.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.subscriptions.service;
 
@@ -30,7 +30,7 @@ public class LDAPUserService
 		CREATE, DELETE
 	}
 
-	@Autowired
+	@Autowired(required = false)
 	private LdapTemplate ldapTemplate;
 
 	public void createVpnAccount(String vpnLoginId, String vpnPassword)
