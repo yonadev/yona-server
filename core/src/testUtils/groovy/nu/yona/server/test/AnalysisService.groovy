49b28cef859add276baaf928bb9b70b3fbf0a76b
Bert-R <bert@famroos.nu>
2017-10-18 09:22:33 +0200

    YD-492 Guarded DB stats through test
    Created a test that guards the database access statistics of the analysis engine service. This service is very performance critical, so it is important that the database access is very limited.
    
    The HibernateStatisticsController is now moved to Core, so each Yona service has the URL path to manage the statistics.

--- core/src/testUtils/groovy/nu/yona/server/test/AnalysisService.groovy
+++ core/src/testUtils/groovy/nu/yona/server/test/AnalysisService.groovy
@@ -6,10 +6,9 @@
  *******************************************************************************/
 package nu.yona.server.test
 
-import groovy.json.*
-
 import java.time.ZonedDateTime
 
+import groovy.json.*
 import nu.yona.server.YonaServer
 
 class AnalysisService extends Service
@@ -40,8 +39,38 @@ class AnalysisService extends Service
 					"url":"$url"
 				}""")
 	}
+
 	def postToAnalysisEngine(String vpnLoginId, jsonString)
 	{
 		yonaServer.postJson(USER_ANONYMIZED_PATH + vpnLoginId + "/networkActivity/", jsonString)
+	}
+	void setEnableStatistics(def enable)
+	{
+		def response = yonaServer.createResource("/hibernateStatistics/enable/", "{}", [:], ["enable" : enable])
+		assert response.status == 200 : "Ensure the server stats are enabled (run with -Dyona.enableHibernateStatsAllowed=true)"
+	}
+
+	def getResource(path, headers = [:], parameters = [:])
+	{
+		yonaServer.getResource(path, headers, parameters)
+	}
+
+	void resetStatistics()
+	{
+		def response = getResource("/hibernateStatistics/", [:], ["reset" : "true"])
+		assert response.status == 200
+	}
+
+	void clearCaches()
+	{
+		def response = yonaServer.createResource("/hibernateStatistics/clearCaches/", "{}", [:], [:])
+		assert response.status == 200
+	}
+
+	def getStatistics()
+	{
+		def response = getResource("/hibernateStatistics/", [:], ["reset" : "false"])
+		assert response.status == 200
+		response.responseData
 	}
 }
