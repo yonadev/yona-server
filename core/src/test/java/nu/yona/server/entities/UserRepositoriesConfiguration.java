84e5c3d3aeb0a64383d6e0cca4acef64be1c2a86
Bert-R <bert@famroos.nu>
2018-01-16 21:31:02 +0100

    YD-528 Updated copyrights

--- core/src/test/java/nu/yona/server/entities/UserRepositoriesConfiguration.java
+++ core/src/test/java/nu/yona/server/entities/UserRepositoriesConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Stichting Yona Foundation
+ * Copyright (c) 2017, 2018 Stichting Yona Foundation
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
