0f299cdb82c9b63122568a25394a08725cba8583
Richard Clark <richard@kerkhofftech.ca>
2018-03-15 11:04:56 -0700

    Kti helm healthchecks (#446)
    * Helm RBAC example
    
    * JNLP Slave in cluster example
    
    * This is a large helm refactor, inc version
    
    * Template updates to support health checks and init containers
    
    The LDAP and Maira URL handling had to change to support urls
    that were not just Springboot/Java centric so they can be used
    for the Init containers to validate those services.
    
    * Add new handling to monitor services now that they have internal health checks
    
    * Update storage class to use 'default'
    
    * Change to the 'standard' storage class.
    This is the normal default storage class on most cluster setups
    and under Minikube removes the need for the k8s/02_storage.yaml
    setup, and makes for a much faster ~4m to ~2m stand up under
    minikube.
    
    At dev-build, there is no storage class even currently defined
    so I am a bit confused how it is even working.  It might only
    be working because it is at an old k8s version.
    
    * Update the ldap package version as there were typos with storage handling
    
    * Add the new wait-for-service category and add to beta and prod
    
    * Add the _ReplaceWithBuildNumber_ back in after testing
    
    * Add hostpath provisioner setup for kubeadm based env (dev-build)

--- scripts/wait-for-services.sh
+++ scripts/wait-for-services.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # Fail on error
 
+_NAMESPACE=${NAMESPACE:-yona}
+
 function waitTillGetWorks() {
 	duration=600
 	sleepTime=5
@@ -21,6 +23,26 @@ function waitTillGetWorks() {
 	return 0
 }
 
+function waitTillK8SInstanceWorks() {
+	duration=${3:-1200}
+	sleepTime=${4:-5}
+	iterations=$[$duration / $sleepTime]
+	n=0
+  echo "Waiting for ${1}-${BUILD_NUMBER_TO_DEPLOY} to become ${2}"
+	until [ $n -ge $iterations ]
+	do
+    kubectl get pods -a --selector=app=${1},build=${BUILD_NUMBER_TO_DEPLOY} -n ${_NAMESPACE} -o jsonpath='{.items[*].status.phase}' | grep -q ${2} && echo -e "\n - Success\n" && break
+    echo -n '.'
+		n=$[$n + 1]
+		sleep $sleepTime
+	done
+	if [ $n -ge $iterations ]
+	then
+		echo "Container ${BUILD_NUMBER_TO_DEPLOY}-${1} failed to become ${2} within timeout ($duration seconds)"
+		return 1
+	fi
+}
+
 # Temporarily use a different approach for Kubernetes
 if [ "$1" == "k8s" ]
 then
@@ -39,6 +61,13 @@ then
 		echo "Failed to get URL $1 within timeout ($duration seconds)"
 		return 1
 	fi
+elif [ "$1" == "k8snew" ]
+then
+	waitTillK8SInstanceWorks liquibase Succeeded 1200 5
+	waitTillK8SInstanceWorks admin Running 60
+	waitTillK8SInstanceWorks analysis Running 60
+	waitTillK8SInstanceWorks app Running 60
+	waitTillK8SInstanceWorks batch Running 60
 else
 	echo "Waiting for the admin service to start"
 	waitTillGetWorks http://127.0.0.1:8080/activityCategories/
