35db145f85b455d0af14dae7ddc0734fef9d4e94
Bert Roos <bert@famroos.nu>
2018-07-03 21:39:35 +0200

    YD-582 Removed useless comments
    It's our simplest class, yet it had the most comments.

--- core/src/main/java/nu/yona/server/rest/ErrorResponseDto.java
+++ core/src/main/java/nu/yona/server/rest/ErrorResponseDto.java
@@ -7,38 +7,19 @@ package nu.yona.server.rest;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * This error response is used to return the error details to the client.
- */
 public class ErrorResponseDto
 {
-	/** The textual error message */
 	private final String message;
 
-	/** The error code. */
 	private final String code;
 
-	/** ID that correlates the server log messages to this error */
 	private final String correlationId;
 
-	/**
-	 * Creates a new error response based on the given message and code.
-	 * 
-	 * @param code The error code.
-	 * @param message The textual error message.
-	 */
 	protected ErrorResponseDto(String code, String message)
 	{
 		this(code, message, null);
 	}
 
-	/**
-	 * Creates a new error response based on the given message, code and correlation ID.
-	 * 
-	 * @param code The error code.
-	 * @param message The textual error message.
-	 * @param correlationId The ID that correlates the server log messages to this error.
-	 */
 	@JsonCreator
 	public ErrorResponseDto(@JsonProperty("code") String code, @JsonProperty("message") String message,
 			@JsonProperty("correlationId") String correlationId)
@@ -48,52 +29,26 @@ public class ErrorResponseDto
 		this.correlationId = correlationId;
 	}
 
-	/**
-	 * Creates a new error response based on the given message and code.
-	 * 
-	 * @param code The error code.
-	 * @param message The textual error message.
-	 */
 	public static ErrorResponseDto createInstance(String code, String message)
 	{
 		return new ErrorResponseDto(code, message, ErrorLoggingFilter.LoggingContext.getCorrelationId());
 	}
 
-	/**
-	 * Creates a new error response based on the given message.
-	 * 
-	 * @param message The textual error message.
-	 */
 	public static ErrorResponseDto createInstance(String message)
 	{
 		return new ErrorResponseDto(null, message, ErrorLoggingFilter.LoggingContext.getCorrelationId());
 	}
 
-	/**
-	 * Returns the error code.
-	 * 
-	 * @return The error code.
-	 */
 	public String getCode()
 	{
 		return code;
 	}
 
-	/**
-	 * Returns the textual error message.
-	 * 
-	 * @return The textual error message.
-	 */
 	public String getMessage()
 	{
 		return message;
 	}
 
-	/**
-	 * Returns the ID that correlates the server log messages to this error.
-	 * 
-	 * @return The ID that correlates the server log messages to this error.
-	 */
 	public String getCorrelationId()
 	{
 		return correlationId;
