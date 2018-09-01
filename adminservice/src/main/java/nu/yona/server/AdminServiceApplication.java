0d5eb0a66ea59c32681201ec080064e82ccaa8e5
Bert Roos <bert@famroos.nu>
2018-09-01 14:25:46 +0200

    YD-586 Enable Micrometer with Prometheus endpoint
    The cache metrics are not exposed yet. Once we've upgraded to Spring 5 (see YD-589), we can extend the current solution with cache metrics (YD-590).

--- adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
+++ adminservice/src/main/java/nu/yona/server/AdminServiceApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * Copyright (c) 2015, 2018 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server;
@@ -7,11 +7,13 @@ package nu.yona.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 import nu.yona.server.properties.PropertyInitializer;
 
 @SpringBootApplication(scanBasePackages = { "nu.yona.server" })
 @EnableCaching
+@EnablePrometheusMetrics
 public class AdminServiceApplication
 {
 	public static void main(String[] args)
