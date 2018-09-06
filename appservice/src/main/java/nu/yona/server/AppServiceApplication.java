52c28d9bb901395254459d93452e81993b224a4c
Bert Roos <Bert-R@users.noreply.github.com>
2018-09-06 21:09:05 +0200

    YD-586 Enable Micrometer with Prometheus endpoint (#498)
    The cache metrics are not exposed yet. Once we've upgraded to Spring 5 (see YD-589), we can extend the current solution with cache metrics (YD-590).

--- appservice/src/main/java/nu/yona/server/AppServiceApplication.java
+++ appservice/src/main/java/nu/yona/server/AppServiceApplication.java
@@ -30,6 +30,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -44,6 +45,7 @@ import nu.yona.server.properties.YonaProperties;
 
 @SpringBootApplication
 @EnableCaching
+@EnablePrometheusMetrics
 public class AppServiceApplication
 {
 	private static final Logger logger = LoggerFactory.getLogger(AppServiceApplication.class);
