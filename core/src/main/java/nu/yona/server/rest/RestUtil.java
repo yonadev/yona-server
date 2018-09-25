0abbb69a46a50a76a9389f21eba1e826df002c1d
Bert Roos <Bert-R@users.noreply.github.com>
2018-09-25 20:37:34 +0200

    YD-594 Invalid UUID in URL results in 400 (#504)
    Introduced RestUtil.parseUuid and used that where we parse UUIDs. That throws an InvalidDataException, resulting in a 400 status code.

--- core/src/main/java/nu/yona/server/rest/RestUtil.java
+++ core/src/main/java/nu/yona/server/rest/RestUtil.java
@@ -1,10 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2016, 2018 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.rest;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
+
+import nu.yona.server.exceptions.InvalidDataException;
 
 public class RestUtil
 {
@@ -17,5 +21,17 @@ public class RestUtil
 	{
 		HttpStatus.Series series = status.series();
 		return (series == HttpStatus.Series.CLIENT_ERROR || series == HttpStatus.Series.SERVER_ERROR);
+	}
+
+	public static UUID parseUuid(String uuid)
+	{
+		try
+		{
+			return UUID.fromString(uuid);
+		}
+		catch (IllegalArgumentException ex)
+		{
+			throw InvalidDataException.invalidUuid(uuid);
+		}
 	}
 }
