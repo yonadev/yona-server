5f21b3d5163c93b932f4328d1ece2d06f5a86092
Bert Roos <bert@famroos.nu>
2019-01-12 17:27:09 +0100

    YD-612 Updated copyright year

--- appservice/src/main/java/nu/yona/server/subscriptions/rest/UserController.java
+++ appservice/src/main/java/nu/yona/server/subscriptions/rest/UserController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2018 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * Copyright (c) 2015, 2019 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.subscriptions.rest;
