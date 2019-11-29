26108cf3bb1c91682479662cf7e5ad0c0157d334
Bert Roos <Bert-R@users.noreply.github.com>
2019-11-29 21:49:52 +0100

    YD-659 Fixed the unit tests
    We now use the Mockito version that comes with the Spring Boot version. Upgraded the equalsverifier to see whether it fixed our issue (it didn't). Then fixed the GoalDtoTest and removed a deprecation warning from DeviceServiceTest.java

--- core/src/test/java/nu/yona/server/goals/service/GoalDtoTest.java
+++ core/src/test/java/nu/yona/server/goals/service/GoalDtoTest.java
@@ -1,13 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 Stichting Yona Foundation
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2018, 2019 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.goals.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.Link;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
@@ -16,7 +14,7 @@ public class GoalDtoTest
 	@Test
 	public void equalsContract()
 	{
-		EqualsVerifier.forClass(GoalDto.class).withRedefinedSuperclass().withOnlyTheseFields("id").withNonnullFields("id")
-				.verify();
+		EqualsVerifier.forClass(GoalDto.class).withRedefinedSuperclass().withOnlyTheseFields("id")
+				.withPrefabValues(Link.class, new Link("edit"), new Link("self")).withNonnullFields("id").verify();
 	}
 }
