e71ce0c1dc50806381f47cc3d053ed4b22283114
Bert Roos <Bert-R@users.noreply.github.com>
2019-12-10 22:21:38 +0100

    Triggering another PR build

--- appservice/src/intTest/groovy/nu/yona/server/DeviceManagementTest.groovy
+++ appservice/src/intTest/groovy/nu/yona/server/DeviceManagementTest.groovy
@@ -2,7 +2,7 @@
  * Copyright (c) 2015, 2019 Stichting Yona Foundation
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v.2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at https://mozilla.org/MPL/2.0/.
+ * obtain one at https://mozilla.org/MPL/2.0/. 
  *******************************************************************************/
 package nu.yona.server
 
