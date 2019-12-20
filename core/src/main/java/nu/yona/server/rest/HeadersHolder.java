354f1b686a5b6cb0369bc27cb60d619caa97b732
Bert Roos <Bert-R@users.noreply.github.com>
2019-12-20 08:34:01 +0100

    YD-669 Addressed SonarQube review comment
    Replaced lambda call with method reference

--- core/src/main/java/nu/yona/server/rest/HeadersHolder.java
+++ core/src/main/java/nu/yona/server/rest/HeadersHolder.java
@@ -1,8 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2019 Stichting Yona Foundation
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v.2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2019 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v.2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.rest;
 
@@ -31,7 +29,7 @@ public class HeadersHolder
 
 	public void writeTo(HttpHeaders headers)
 	{
-		storedHeaders.forEach((n, v) -> headers.add(n, v));
+		storedHeaders.forEach(headers::add);
 	}
 
 	public void importFrom(Map<String, String> headersToImport)
