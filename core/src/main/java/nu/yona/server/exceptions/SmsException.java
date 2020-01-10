2efb055023405cf2f0921dc4321f506e5b576972
Bert Roos <Bert-R@users.noreply.github.com>
2020-01-10 08:15:31 +0100

    YD-674 SmsException results in INTERNAL_SERVER_ERROR

--- core/src/main/java/nu/yona/server/exceptions/SmsException.java
+++ core/src/main/java/nu/yona/server/exceptions/SmsException.java
@@ -1,10 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License, v.
- * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2016, 2020 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server.exceptions;
 
 import java.io.Serializable;
+
+import org.springframework.http.HttpStatus;
 
 /**
  * This exception is to be used when email sending fails.
@@ -15,12 +17,12 @@ public class SmsException extends YonaException
 
 	private SmsException(String messageId, Serializable... parameters)
 	{
-		super(messageId, parameters);
+		super(HttpStatus.INTERNAL_SERVER_ERROR, messageId, parameters);
 	}
 
 	private SmsException(Throwable t, String messageId, Serializable... parameters)
 	{
-		super(t, messageId, parameters);
+		super(HttpStatus.INTERNAL_SERVER_ERROR, t, messageId, parameters);
 	}
 
 	public static SmsException smsSendingFailed(Exception cause)
