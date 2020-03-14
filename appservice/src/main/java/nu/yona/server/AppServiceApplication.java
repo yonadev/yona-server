77a26703050a58715e48e76befd9116ec87b8afb
Bert Roos <Bert-R@users.noreply.github.com>
2020-03-14 19:11:20 +0100

    YD-684 Log startup failures and shutdown

--- appservice/src/main/java/nu/yona/server/AppServiceApplication.java
+++ appservice/src/main/java/nu/yona/server/AppServiceApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2018 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * Copyright (c) 2015, 2020 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server;
@@ -28,7 +28,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -57,7 +60,23 @@ public class AppServiceApplication implements WebMvcConfigurer
 	public static void main(String[] args)
 	{
 		PropertyInitializer.initializePropertiesFromEnvironment();
-		SpringApplication.run(AppServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AppServiceApplication.class, args);
+		addLoggerForContextClosedEvent(context);
+	}
+
+	private static void addLoggerForContextClosedEvent(ConfigurableApplicationContext context)
+	{
+		context.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
+			@Override
+			public void onApplicationEvent(ContextClosedEvent event)
+			{
+				if (event.getApplicationContext() != context)
+				{
+					return;
+				}
+				logger.info(Constants.ALERT_MARKER, "Application closed");
+			}
+		});
 	}
 
 	@PostConstruct
