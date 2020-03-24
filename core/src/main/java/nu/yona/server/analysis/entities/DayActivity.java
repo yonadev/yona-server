e177b862fef83d4932976b2746d071603072bd84
Bert Roos <Bert-R@users.noreply.github.com>
2020-03-24 20:12:27 +0100

    Reverted test commit

--- core/src/main/java/nu/yona/server/analysis/entities/DayActivity.java
+++ core/src/main/java/nu/yona/server/analysis/entities/DayActivity.java
@@ -38,12 +38,6 @@ import nu.yona.server.util.TimeUtil;
 @Entity
 public class DayActivity extends IntervalActivity
 {
-	static {
-		Optional<Object> oo = Optional.empty();
-		Object o = oo.get();
-		System.out.println("Optional contains: " + o); // See whether SonarQube triggers on this.
-	}
-
 	@ManyToOne(fetch = FetchType.LAZY)
 	private WeekActivity weekActivity;
 
