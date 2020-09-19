0645f4127c082cd8b83eda733c40e3150c3ad2f1
Bert Roos <Bert-R@users.noreply.github.com>
2020-09-19 20:18:48 +0200

    YD-684 Log startup failures and shutdown (#594)
    * YD-684 Log startup failures and shutdown
    
    * YD-684 Implemented app close logging for all services
    
    * YD-684 Removed unused imports

--- analysisservice/src/main/java/nu/yona/server/AnalysisServiceApplication.java
+++ analysisservice/src/main/java/nu/yona/server/AnalysisServiceApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2018 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
+ * Copyright (c) 2015, 2020 Stichting Yona Foundation This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *******************************************************************************/
 package nu.yona.server;
@@ -9,6 +9,7 @@ import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import nu.yona.server.properties.PropertyInitializer;
@@ -21,7 +22,8 @@ public class AnalysisServiceApplication
 	public static void main(String[] args)
 	{
 		PropertyInitializer.initializePropertiesFromEnvironment();
-		SpringApplication.run(AnalysisServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AnalysisServiceApplication.class, args);
+		ApplicationStatusLogger.addLoggerForContextClosedEvent(context);
 	}
 
 	@Bean
