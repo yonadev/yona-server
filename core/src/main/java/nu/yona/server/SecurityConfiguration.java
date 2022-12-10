01c77c29435724de889b90b1b51df3720c901e30
Bert Roos <Bert-R@users.noreply.github.com>
2022-12-10 17:59:02 +0100

    Remove unused imports

--- core/src/main/java/nu/yona/server/SecurityConfiguration.java
+++ core/src/main/java/nu/yona/server/SecurityConfiguration.java
@@ -4,15 +4,11 @@
  *******************************************************************************/
 package nu.yona.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
